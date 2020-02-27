a868a31fa37fec9f9e3888951840d56c4d7619d3
Walter Wan <walter@trayt.io>
2020-02-26 21:44:59 -0800

    Fix publish

--- src/helper.js
+++ src/helper.js
@@ -3,7 +3,7 @@
 const BIGINT_0 = BigInt(0)
 const BIGINT_1 = BigInt(1)
 
-const longToIp = (long) => {
+export const longToIp = (long) => {
   const a = (long & (0xff << 24)) >>> 24
   const b = (long & (0xff << 16)) >>> 16
   const c = (long & (0xff << 8)) >>> 8
@@ -11,7 +11,7 @@ const longToIp = (long) => {
   return [a, b, c, d].join('.')
 }
 
-const bigintToIpv6 = (bigint) => {
+export const bigintToIpv6 = (bigint) => {
   const ipv6Arr = []
   for (let i = BIGINT_0; i < BigInt(8); i++) {
     const shift = BigInt(112) - (BigInt(16) * i)
@@ -21,7 +21,7 @@ const bigintToIpv6 = (bigint) => {
   return ipv6Arr.map((decimal) => decimal.toString(16)).join(':')
 }
 
-const ipTolong = (ip) => {
+export const ipTolong = (ip) => {
   const b = ip.split('.')
   if (b.length === 0 || b.length > 4) {
     throw new Error('Invalid IP')
@@ -38,7 +38,7 @@ const ipTolong = (ip) => {
   return ((b[0] << 24) | (b[1] << 16) | (b[2] << 8) | b[3]) >>> 0
 }
 
-const ipv6ToBigint = (ipv6) => {
+export const ipv6ToBigint = (ipv6) => {
   const arr = ipv6.split(':')
   if (arr.length !== 8) {
     throw new Error(`Invalid IPv6: ${ipv6}`)
@@ -59,7 +59,7 @@ const ipv6ToBigint = (ipv6) => {
   return temp >> BIGINT_0
 }
 
-const printBigObj = (pojo) => {
+export const printBigObj = (pojo) => {
   const newPojo = {}
   Object.entries(pojo).forEach(([key, val]) => {
     if (typeof val === "bigint") newPojo[key] = val.toString()
@@ -69,7 +69,7 @@ const printBigObj = (pojo) => {
   return newPojo
 }
 
-const BigIntPow = (base, exp) => {
+export const BigIntPow = (base, exp) => {
   if (exp === BIGINT_0) {
     return BIGINT_1
   } else if (exp === BIGINT_1) {
@@ -81,7 +81,7 @@ const BigIntPow = (base, exp) => {
   return BigIntPow(base, exp - BIGINT_1) * BigIntPow(base, BIGINT_1)
 }
 
-const unShortenIpv6 = (shortenedIpv6) => {
+export const unShortenIpv6 = (shortenedIpv6) => {
   const ipArr = shortenedIpv6.split('::')
   if (ipArr.length > 2) {
     throw new Error(`Invalid ipv6 address: ${shortenedIpv6}`)
@@ -97,14 +97,4 @@ const unShortenIpv6 = (shortenedIpv6) => {
   const ipv6Arr = [...leftIpArr, ...Array(hexGroupToAdd).fill('0'), ...rightIpArr]
 
   return ipv6Arr.filter((hex) => hex !== '').join(':')
-}
-
-module.exports = {
-  longToIp,
-  bigintToIpv6,
-  ipTolong,
-  ipv6ToBigint,
-  BigIntPow,
-  unShortenIpv6,
-  printBigObj,
 }
