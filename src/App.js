32bd86e5b791c9927fcaa0d362cf25f62145efcf
Walter Wan <WalterHWan@gmail.com>
2020-02-27 09:58:14 -0800

    Add ipv4mappedIpv6 feature

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import BinaryExplained from './BinaryExplained'
 import './App.css'
 
 const DEFAULT_CIDR = '192.168.0.5/31'
+const reIpv4mapped = /(?<=^::ffff:)((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])$/
 
 function decimalToBinary(dec) {
   return Number(dec)
@@ -22,6 +23,25 @@ function ipv4ToBinary(ip) {
     .join(' ')
 }
 
+const decimalToHex = (decimal) => {
+  return parseInt(decimal, 10).toString(16)
+}
+
+const ipv4ToIpv4mappedIpv6Hex = (ivp4) => {
+  const arr =  ivp4.split('.').map(decimalToHex)
+  return `::ffff:${arr[0] + arr[1]}:${arr[2] + arr[3]}`
+}
+
+const isExactIpv6 = (string) => 
+  ipRegex.v6({ exact: true }).test(string)
+
+const isExactIpv4 = (string) => 
+  ipRegex.v4({ exact: true }).test(string)
+
+const isExactIpv4mapped = (string) => {
+  return reIpv4mapped.test(string)
+}
+
 function cidrToDisplay(cidr, errorCallBack) {
   if (!cidr) return {}
   let block
@@ -30,7 +50,7 @@ function cidrToDisplay(cidr, errorCallBack) {
   if (cidr) {
     try {
       const [ip] = cidr.split('/')
-      if (ipRegex.v6({ exact: true }).test(ip)) {
+      if (isExactIpv6(ip)) {
         block = new Netmask6(cidr)
       } else {
         block = new Netmask4(cidr)
@@ -74,20 +94,45 @@ function App() {
   const [inputBox, setInputBox] = React.useState(DEFAULT_CIDR)
   const [cidr, setCidr] = React.useState(inputBox)
   const [errorText, setErrorText] = React.useState('')
-  const [isIpv6, setIsIpv6] = React.useState(ipRegex.v6({ exact: true }).test(DEFAULT_CIDR))
-
+  const [isIpv6, setIsIpv6] = React.useState(isExactIpv6(DEFAULT_CIDR))
+  const [ipVersion, setIpVersion] = React.useState('IPv4')
+  const [timer, setTimer] = React.useState()
+  
   const handleChange = (event) => {
     const value = event.target.value
-    const [ip, bitmask] = value.split('/')
-    const isIpv4 = ipRegex({ exact: true }).test(ip)
-    setIsIpv6(ipRegex.v6({ exact: true }).test(ip))
+    const [_ip, bitmask] = value.split('/')
+    let ip = _ip
 
-    if (isIpv6 && bitmask) setCidr(value)
-    else if (!isIpv6 && isIpv4 && bitmask) setCidr(value)
-    else setCidr()
+    // deal with ipv4 mapped ipv6
+    if (isExactIpv4mapped(_ip)) {
+      const v4 = _ip.match(reIpv4mapped)[0]
+      ip = ipv4ToIpv4mappedIpv6Hex(v4)
+      setIpVersion('IPv4-Mapped IPv6')
+      setIsIpv6(true)
+      if (bitmask) setCidr(`${ip}/${bitmask}`)
+    } else if (isExactIpv6(ip)) {
+      if (bitmask) setCidr(`${ip}/${bitmask}`)
+      setIsIpv6(true)
+      setIpVersion('IPv6')
+    } else if (isExactIpv4(ip)) {
+      if (bitmask) setCidr(`${ip}/${bitmask}`)
+      setIpVersion('IPv4')
+    } else {
+      setIpVersion()
+      setCidr()
+    }
 
     setInputBox(value)
     setErrorText('')
+    
+    // purge not allowed charactors
+    const re = /[^/0-9a-fA-F:.]|(?<=\.)\.+|(?<=::):+/gm
+    clearTimeout(timer)
+    setTimer(setTimeout(() => {
+      const newCidr = value.replace(re, '')
+      setInputBox(newCidr)
+      setCidr(newCidr)
+    }, 2000))
   }
 
   const handleError = (err) => {
@@ -111,6 +156,7 @@ function App() {
         value={inputBox}
         onChange={handleChange}
       />
+      {ipVersion && <p>{`Detected IP version: ${ipVersion}`}</p>}
       {/* Data/Error */}
       {!cidr ? (
         <div className="cidr-error">
